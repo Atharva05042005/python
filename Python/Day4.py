7447cd6c87f6ce53b4f16ed633da4eea61e95dd5
Atharva05042005 <divekaratharva6@gmail.com>
2025-06-14 00:11:28 +0530

    add

--- Python/Day4.py
+++ Python/Day4.py
@@ -1,6 +1,7 @@
-a='harry  !!!!'
+a='harry Is Good !!!!'
 print(a.upper())
 print(a.lower())
 print(a.rstrip("!"))
-print(a.replace("Harry!!!!","Jonh"))
+print(a.replace("harry","Jonh"))
 #print(a.split(" "))
+print(a.capitalize())
