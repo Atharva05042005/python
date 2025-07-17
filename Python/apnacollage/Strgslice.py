6593da898284990e55b0f6cdbe342d998a0ee9d7
Atharva05042005 <divekaratharva6@gmail.com>
2025-07-17 22:27:07 +0530

    add

--- Python/apnacollage/Strgslice.py
+++ Python/apnacollage/Strgslice.py
@@ -1,2 +1,9 @@
-str="I am a coder"
+str="i am a coder"
+#print(str.capitalize)#This will work only one time
+str= str.capitalize()
 print(str.endswith("er"))
+print(str)
+print(str.replace("coder","devloper"))
+print(str.find("am"))
+print(str.count("a"))
+str.
