922d111659201182ce2b764f4a5f00e34430159a
Atharva05042005 <divekaratharva6@gmail.com>
2025-07-17 22:52:59 +0530

    add

---
+++ Python/apnacollage/conditional statement.py
@@ -0,0 +1,19 @@
+# light="red"
+# if(light=="red"):#indentation
+#     print("stop")
+# elif(light=="orange"):
+#     print("Start your engin")
+# elif(light=="green"):
+#     print("go")
+# else:
+#     print("light is broken")
+
+#nesting
+Age=80
+if(Age>18):
+    if(Age>=80):
+        print ("you cannot drive")
+    else:
+        print("you can Drive")
+else:
+    print("you cannot Drive")
