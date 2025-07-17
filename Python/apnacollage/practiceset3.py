352765bb367dbd8ac17e16d632baa394a6e91815
Atharva05042005 <divekaratharva6@gmail.com>
2025-07-18 02:16:17 +0530

    add

--- Python/apnacollage/practiceset3.py
+++ Python/apnacollage/practiceset3.py
@@ -6,11 +6,16 @@
 # print(movie)
 
 
-lit1=[2,2,2]
-copy_lit1=lit1.copy()
-lit1.reverse()
-print(lit1)
-if(lit1==copy_lit1):
-    print("list is tuple")
-else:
-    print("list is not a tuple")
+# lit1=[2,2,2]
+# copy_lit1=lit1.copy()
+# lit1.reverse()
+# print(lit1)
+# if(lit1==copy_lit1):
+#     print("list is palindrom")
+# else:
+#     print("list is not a palindrom")
+
+
+tup=["A","B","C","A","B","C","A","A","B"]
+print(tup.sort())
+print(tup)
