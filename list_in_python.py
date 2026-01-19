a125ac85322293307c78c54433a3201b7c2fce02
Atharva05042005 <divekaratharva6@gmail.com>
2026-01-19 23:24:23 +0530

    Add files via upload

---
+++ list_in_python.py
@@ -0,0 +1,15 @@
+student="Atharva"
+mark=80
+rollno= 25
+
+list1=["atharva",80,25]
+list2=[rollno,mark,list1]
+print(len(list1))
+print(list1[0])
+print(list1[1:3:2])
+print(list1[1:3:2])
+print(list1+list2)
+list1.append(43.3)
+print(list1)
+list1.insert(0,"Atharva")
+print(list1)
